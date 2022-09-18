729ab296cc2f273aca256f8797eaf2b5dc7e5b42
MellyMel Melnyk <113805475+Snowflake20220916@users.noreply.github.com>
2022-09-18 14:50:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,7 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(fruityvice_normalized)
+streamlist.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
